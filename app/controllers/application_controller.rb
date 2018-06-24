f65439bfa65f4d20fe0b8304ffbdddd7bad8e50a
Cristian Faune <cristianfaune.t@gmail.com>
2018-06-24 13:03:16 -0400

    fix devise access problems

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-  protect_from_forgery with: :exception
+  # before_action :authenticate_admin!
+  protect_from_forgery prepend: true
 
   protected
 
